d8d7e083539d8d3b7596e98991c57f5a7ea69259
MD js <minhduc.javascript@gmail.com>
2024-04-10 19:41:43 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const ejs = require("ejs"); // Important
+
+const app = express();
+
+// Set template engine
+app.set("view engine", "ejs");
+
+// Đăng ký thư mục public
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact");
+});
+
+app.get("/post", (req, res) => {
+    res.render("post");
+});
+
+app.listen(5000, () => {
+    console.log("Go to http://localhost:5000");
+});

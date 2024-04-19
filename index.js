a03b0198d98a934edb1ed76ce4561205ab049d35
MD js <minhduc.javascript@gmail.com>
2024-04-19 20:20:14 +0700

    first route - create new post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("ejs"); // Important
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const {
     addPost,
     getPost,
@@ -21,14 +22,40 @@ try {
     console.log("Cannot connect MongoDB", error);
 }
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Đăng ký thư mục public
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-    const post = await updatePost('12c', {
-        body: "Something....",
-    });
-    console.log("post:", post);
+// GET, POST, PUT/PATCH, DELETE (CRUD)
+app.get("/posts/new", async (req, res) => {
+    // open create.ejs page
+    res.render("create");
+});
+
+app.post("/posts/store", async (req, res) => {
+    // const title = req.body.title;
+    // const body = req.body.body;
+    const { title, body } = req.body; // for short
+
+    try {
+        const newPost = await addPost(title, body);
+
+        res.status(200).json({
+            status: "success",
+            data: newPost,
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: "error",
+            // error: error,
+            error, // for short
+        });
+    }
+});
+
+app.get("/", (req, res) => {
     res.render("index");
 });
 
--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    title: String,
-    body: String,
+    title: {
+        type: String,
+        required: [true, "Title is required, please fill the title!"],
+    },
+    body: {
+        type: String,
+        required: [true, "Body is required, please fill the body!"],
+    },
 });
 
 const PostModel = mongoose.model("Post", postSchema);
